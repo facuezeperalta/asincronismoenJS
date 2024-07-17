118ca26a9edef8f08db137b7b2cfeb566c2f8a82
Facundo Peralta <facundoperaltaezequiel@gmail.com>
2024-07-17 20:14:41 -0300

    post terminado

--- src/promise/challenge-post.js
+++ src/promise/challenge-post.js
@@ -1,12 +1,28 @@
 const API = 'https://api.escuelajs.co/api/v1';
 
-function postData(urlApi,data){
-    const response = fetch(urlApi,{
+//post data, agregar información a la API
+
+function postData(api,data){
+    const response = fetch(api,{
         method: 'POST',
-        mode: 'cors',
-        credentials:'same-origin',
-        headers: {
-            'Content-type': 'apli'
-        } 
-    })
+        mode:'cors',
+        credentials: 'same-origin',
+        headers:{
+            'Content-Type':'application/json'
+        },
+        body: JSON.stringify(data)
+    });
+    return response;
 }
+
+const data = {
+    "title": "Nizon Z6 III",
+    "price": 2800,
+    "description": "Full frame Mirrorless camera",
+    "categoryId": 1,
+    "images": ["https://placeimg.com/640/480/any"]
+  }
+
+postData(`${API}/products`,data)
+  .then(response => response.json())
+  .then(data => console.log(data));
